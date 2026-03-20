6c0a6050e7c3732aeeff093f1ac8e51f1c8563e8
deantook <deantook@outllook.com>
2026-03-21 03:48:45 +0800

    style(domain): 移除实体类中的TableField注解
    - 从Credit实体类中移除TableField注解
    - 从Person实体类中移除TableField注解
    - 从Role实体类中移除TableField注解
    - 从UserItemStatu实体类中移除TableField注解
    - 从UserProgres实体类中移除TableField注解
    - 从UserReview实体类中移除TableField注解

--- src/main/java/io/github/deantook/taco/domain/UserReview.java
+++ src/main/java/io/github/deantook/taco/domain/UserReview.java
@@ -1,7 +1,6 @@
 package io.github.deantook.taco.domain;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.math.BigDecimal;
